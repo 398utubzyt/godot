939372dbe9d21adbc1989553c1555087b01758fe
398utubzyt <398utubzyt@gmail.com>
2023-07-04 18:52:55 -0700

    Remove documentation check

--- modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/GlobalClassAnalyzer.cs
+++ modules/mono/editor/Godot.NET.Sdk/Godot.SourceGenerators/GlobalClassAnalyzer.cs
@@ -25,9 +25,6 @@ namespace Godot.SourceGenerators
 
         private void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
-            // Ignore syntax inside comments
-            if (IsInsideDocumentation(context.Node))
-                return;
             var typeClassDecl = (ClassDeclarationSyntax)context.Node;
 
             // Return if not a type symbol or the type is not a global class.
@@ -40,26 +37,6 @@ namespace Godot.SourceGenerators
 
             if (!typeSymbol.InheritsFrom("GodotSharp", GodotClasses.GodotObject))
                 Common.ReportGlobalClassMustDeriveFromGodotObject(context, typeClassDecl, typeSymbol);
-        }
-
-        /// <summary>
-        /// Check if the syntax node is inside a documentation syntax.
-        /// </summary>
-        /// <param name="syntax">Syntax node to check.</param>
-        /// <returns><see langword="true"/> if the syntax node is inside a documentation syntax.</returns>
-        private bool IsInsideDocumentation(SyntaxNode? syntax)
-        {
-            while (syntax != null)
-            {
-                if (syntax is DocumentationCommentTriviaSyntax)
-                {
-                    return true;
-                }
-
-                syntax = syntax.Parent;
-            }
-
-            return false;
         }
     }
 }
